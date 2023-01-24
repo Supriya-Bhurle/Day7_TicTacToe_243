382e629d0bcfd19ca732ae9837eb718de8470d5f
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-01-24 21:08:27 +0530

    Added : Empty board added

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -1,2 +1,19 @@
-package com.bridgelabz.tictactoe;public class TicTacToe {
+package com.bridgelabz.tictactoe;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        char[][] gameBoard = {{'_', '|', '_', '|', '_'}, {'_', '|', '_', '|', '_'}, {'_', '|', '_', '|', '_'}};
+        printBOARD(gameBoard);
+        
+    }
+
+    private static void printBOARD(char[][] gameBoard) {
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard[i].length; j++) {
+                System.out.print(gameBoard[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+    }
 }
